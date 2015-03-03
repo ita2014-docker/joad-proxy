1cd034c66c30654fd4af2fd0eea2fd4c80cd2e40
Takahiro Yano <yano_takahiro@ogis-ri.co.jp>
2015-03-03 17:29:36 +0900

    first implementation

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
 FROM matsumotory/ngx-mruby
 
+ENV DOCKER_SERVER_IP 172.17.42.1
+ENV PROXY_DOMAIN example.com
+
 # forward request and error logs to docker log collector
-RUN ln -sf /dev/stdout /var/log/nginx/access.log
-RUN ln -sf /dev/stderr /var/log/nginx/error.log
+RUN ln -sf /dev/stdout /usr/local/nginx/logs/access.log
+RUN ln -sf /dev/stderr /usr/local/nginx/logs/error.log
 
 ADD ./run.sh /build/
 CMD ["/build/run.sh"]
--- docker/hook/proxy.rb
+++ docker/hook/proxy.rb
@@ -1,11 +1,25 @@
+DOCKER_SERVER_IP = ENV["DOCKER_SERVER_IP"] || "172.17.42.1"
+PROXY_DOMAIN = ENV["PROXY_DOMAIN"] || "example.com"
+
+hostname = Nginx::Var.new.http_host
+app = hostname.split(".#{PROXY_DOMAIN}").first
+
 begin
   s = UNIXSocket.open("/var/run/docker.sock")
-  s.write("GET /containers/json HTTP/1.0\r\n\r\n")
+  s.write("GET /containers/#{app}/json HTTP/1.0\r\n\r\n")
 
   until s.gets.chomp.empty?
   end
 
-  Nginx.echo s.read
+  json = JSON::parse(s.read)
 ensure
   s.close
 end
+
+ports = json["NetworkSettings"]["Ports"]
+host_port = nil
+%w{ 80/tcp 8080/tcp 8000/tcp 3000/tcp }.each do |p|
+  host_port = ports[p].first["HostPort"] if ports.keys.include?(p)
+  break if host_port
+end
+"#{DOCKER_SERVER_IP}:#{host_port}" if host_port
